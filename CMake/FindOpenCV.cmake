e0fcf51b3dcfe2c4fda824d2d29c35d909a51730
marcus <marcus.edel@fu-berlin.de>
2016-03-14 18:02:37 +0100

    Link against opencv_imgcodecs if opencv 3.

--- CMake/FindOpenCV.cmake
+++ CMake/FindOpenCV.cmake
@@ -13,7 +13,23 @@
 
 include( FindPackageHandleStandardArgs )
 
-set( OpenCV_VERSION %%OCV_VERSION%% )
+find_file(OPENCV_CONFIG_CMAKE NAMES OpenCVConfig.cmake
+  PATHS
+  ${OPENCV_CMAKE_PATHS}
+)
+
+if(EXISTS ${OPENCV_CONFIG_CMAKE})
+    include(${OPENCV_CONFIG_CMAKE})
+endif()
+
+find_file(OPENCV_CONFIG_VERSION_CMAKE NAMES OpenCVConfig-version.cmake
+  PATHS
+  ${OPENCV_CMAKE_PATHS}
+)
+
+if(EXISTS ${OPENCV_CONFIG_VERSION_CMAKE})
+    include(${OPENCV_CONFIG_VERSION_CMAKE})
+endif()
 
 find_path( OpenCV_INCLUDE_DIRS NAMES opencv2/core/core.hpp )
 
@@ -30,9 +46,14 @@ find_library( OPENCV_CORE_LIBRARY NAMES opencv_core )
 if( OPENCV_USE_IMGPROC OR NOT OpenCVCore_FIND_COMPONENTS )
   find_library( OPENCV_IMGPROC_LIBRARY NAMES opencv_imgproc )
   find_library( OPENCV_HIGHGUI_LIBRARY NAMES opencv_highgui )
-endif()
 
-set( OpenCV_LIBS ${OPENCV_CORE_LIBRARY} ${OPENCV_IMGPROC_LIBRARY} ${OPENCV_HIGHGUI_LIBRARY})
+  if(OpenCV_VERSION VERSION_LESS "3.0.0")
+    set( OpenCV_LIBS ${OPENCV_CORE_LIBRARY} ${OPENCV_IMGPROC_LIBRARY} ${OPENCV_HIGHGUI_LIBRARY} )
+  else()
+    find_library( OPENCV_IMGCODECS_LIBRARY NAMES opencv_imgcodecs )
+    set( OpenCV_LIBS ${OPENCV_CORE_LIBRARY} ${OPENCV_IMGPROC_LIBRARY} ${OPENCV_HIGHGUI_LIBRARY} ${OPENCV_IMGCODECS_LIBRARY} )
+  endif()
+endif()
 
 find_package_handle_standard_args( OpenCVCore DEFAULT_MSG OpenCV_LIBS OpenCV_INCLUDE_DIRS )
 
